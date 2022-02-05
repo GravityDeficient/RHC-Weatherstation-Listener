858ba06556b8a5e88f8ea35778a5ac5c83ee6c7a
Andrew Misplon <amisplon@gmail.com>
2022-02-05 14:38:30 +0200

    Updated the PWS pressure calculation

--- services/pwsweather.js
+++ services/pwsweather.js
@@ -20,7 +20,7 @@ let pwsweather = {
             this.query.tempf = pdata.temp;
         }
         if(pdata.pres !== undefined) {
-            this.query.baromin = pdata.pres/3386;
+            this.query.baromin = (pdata.pres / 10) * 0.02952999;
         }
         if(pdata.hum !== undefined) {
             this.query.humidity = pdata.hum;
