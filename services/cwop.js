aac191f5174d52a71dd8be4281c95cae1eacae15
Shane Easton <shane.easton@gmail.com>
2023-01-29 22:04:56 -0800

    updated cwop service

--- services/cwop.js
+++ services/cwop.js
@@ -1,46 +1,51 @@
-const request = require('request');
 const net = require('net');
 let cwop = {
-    host: 'cwop.aprs.net',
-    port: 14580,
-    packet: {
-        ID: 0,
-        PASSWORD: 0,
-        winddir: 0,
-        windspeedmph: 0,
-        windgustmph: 0,
-    },
-    login: function(id, password) {
-        this.query.ID = id;
-        this.query.PASSWORD = password;
-    },
-    update: function(pdata) {
-        this.query.winddir = pdata.dir;
-        this.query.windspeedmph = pdata.wind;
-        this.query.windgustmph = pdata.gust;
-        if(pdata.temp !== undefined) {
-            this.query.tempf = pdata.temp;
-        }
-        if(pdata.pres !== undefined) {
-            this.query.baromin = pdata.pres/3386;
-        }
-        if(pdata.hum !== undefined) {
-            this.query.humidity = pdata.hum;
-        }
-        if(pdata.dewp !== undefined) {
-            this.query.dewptf = pdata.dewp;
-        }
-    },
-    send: function() {
-        const client = new Net.Socket();
-        client.connect({ port: this.port, host: this.host }, function() {
-            client.write('Hello, server.');
-        });
-        client.on('end', function() {
-            console.log('Requested an end to the TCP connection');
-        });
+  host: 'cwop.aprs.net',
+  port: 14580,
+  ID: 0,
+  PASSWORD: 0,
+  query: "",
 
-        console.log("CWOP/APRS sent..");
+  login: function(id, password) {
+    this.ID = id;
+    this.PASSWORD = password;
+  },
+
+  update: function(pdata) {
+    const timestamp = Math.floor(Date.now() / 1000);
+
+    this.query = `user ${this.ID} pass ${this.PASSWORD} vers NodeJS-0.1\n`;
+    this.query += `weather ${this.ID} "@${timestamp}z ${pdata.winddir}/${pdata.windspeedmph}g${pdata.windgustmph}`;
+
+    if (pdata.temp !== undefined) {
+      this.query += `t${pdata.temp}`;
     }
+    if (pdata.dewp !== undefined) {
+      this.query += `d${pdata.dewp}`;
+    }
+    if (pdata.hum !== undefined) {
+      this.query += `h${pdata.hum}`;
+    }
+    if (pdata.pres !== undefined) {
+      this.query += `b${pdata.pres}`;
+    }
+
+    this.query += "\n";
+  },
+
+  send: function() {
+    const client = new Net.Socket();
+    client.connect({ port: this.port, host: this.host }, function() {
+      client.write(this.query);
+      client.end();
+    });
+
+    client.on('end', function() {
+      console.log('Requested an end to the TCP connection');
+    });
+
+    console.log("CWOP/APRS sent..");
+  }
 };
+
 module.exports = cwop;
